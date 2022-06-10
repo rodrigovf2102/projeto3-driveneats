d61cfae025168c13214cee1abc1c5d8c3d1dc9e7
rodrigovf2102 <rodrigovf2102@gmail.com>
2022-06-10 14:11:07 -0300

    envio de pedido para wpp implementado

--- script.js
+++ script.js
@@ -323,3 +323,40 @@ function escolherSobremesa3(){
         caixabotao.classList.remove("colorirDeVerde");
     }
 }
+function wpp(){
+    const element = document.querySelector(".colorirDeVerde")
+    const comidas = document.querySelectorAll(".opcao");
+    let url = "https://wa.me/5531991929224?text=Olá,";
+    let texto ="gostaria de fazer um pedido:\n\- Prato: ";
+    let total=0.0;
+    for(let i=0;i<3;i++){
+        
+      if(comidas[i].classList.contains("box-escolhido")){
+          if(i==0){texto+="Frango Yin Yang\n";total+=14.90;}
+          if(i==1){texto+="Quarteirão\n";total+=12.90;}
+          if(i==2){texto+="Whopper\n";total+=13.90;}
+      }  
+    }
+    texto+="- Bebida: ";
+    for(let i=3;i<6;i++){
+        if(comidas[i].classList.contains("box-escolhido")){
+            if(i==3){texto+="Coquinha Gelada\n";total+=4.90;}
+            if(i==4){texto+="Coca do MC\n";total+=8.90;}
+            if(i==5){texto+="Pepsi do Bk\n";total+=7.90;}
+        }  
+      }
+      texto+="- Sobremesa: ";   
+      for(let i=6;i<9;i++){
+        if(comidas[i].classList.contains("box-escolhido")){
+            if(i==6){texto+="Pudim\n";total+=7.90;}
+            if(i==7){texto+="Top-Sundae\n";total+=8.90;}
+            if(i==8){texto+="Balde-Ovomaltine\n";total+=10.90;}
+        }  
+      }
+      texto+="Total: R$ "+total.toFixed(2);
+      url = url + encodeURIComponent(texto);
+    if(element!=null){
+        window.open(url);
+    }
+    
+}
