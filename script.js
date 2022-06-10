ea1cf9e7681767ec53cd0549ca0944dea4d4be51
rodrigovf2102 <rodrigovf2102@gmail.com>
2022-06-10 10:24:21 -0300

    adicionado resposividade de bebidas

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ function escolherPrato1(){
     const element = document.querySelector(".opcao");
     element.classList.toggle("box-escolhido");
 }
-
 function escolherPrato2(){
     const element = document.querySelectorAll(".opcao");
     element[1].classList.toggle("box-escolhido");
 }
-
 function escolherPrato3(){
     const element = document.querySelectorAll(".opcao");
     element[2].classList.toggle("box-escolhido");
@@ -22,5 +20,17 @@ function escolherBebida2(){
 }
 function escolherBebida3(){
     const element = document.querySelectorAll(".opcao");
-    element[4].classList.toggle("box-escolhido");
+    element[5].classList.toggle("box-escolhido");
+}
+function escolherSobremesa1(){
+    const element = document.querySelectorAll(".opcao");
+    element[6].classList.toggle("box-escolhido");
+}
+function escolherSobremesa2(){
+    const element = document.querySelectorAll(".opcao");
+    element[7].classList.toggle("box-escolhido");
+}
+function escolherSobremesa3(){
+    const element = document.querySelectorAll(".opcao");
+    element[8].classList.toggle("box-escolhido");
 }
