6329e0854999398ebe160f15ca66e7c74820fbeb
rodrigovf2102 <rodrigovf2102@gmail.com>
2022-06-10 10:16:32 -0300

    adicionado responsividade de comidas

---
+++ script.js
@@ -0,0 +1,26 @@
+function escolherPrato1(){
+    const element = document.querySelector(".opcao");
+    element.classList.toggle("box-escolhido");
+}
+
+function escolherPrato2(){
+    const element = document.querySelectorAll(".opcao");
+    element[1].classList.toggle("box-escolhido");
+}
+
+function escolherPrato3(){
+    const element = document.querySelectorAll(".opcao");
+    element[2].classList.toggle("box-escolhido");
+}
+function escolherBebida1(){
+    const element = document.querySelectorAll(".opcao");
+    element[3].classList.toggle("box-escolhido");
+}
+function escolherBebida2(){
+    const element = document.querySelectorAll(".opcao");
+    element[4].classList.toggle("box-escolhido");
+}
+function escolherBebida3(){
+    const element = document.querySelectorAll(".opcao");
+    element[4].classList.toggle("box-escolhido");
+}
