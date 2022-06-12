4ab398c5bbb0c1dcfd9bd4dadcbf9fb433a9022e
rodrigovf2102 <rodrigovf2102@gmail.com>
2022-06-12 20:35:39 -0300

    melhorias na lógica do código

--- script.js
+++ script.js
@@ -1,80 +1,92 @@
-function escolherPrato(element){
+function escolherPrato(element) {
     const selecionado = document.querySelector(".comida.box-escolhido");
-    if(selecionado!=null){
+    if (selecionado != null) {
         selecionado.classList.remove("box-escolhido");
     }
     element.classList.add("box-escolhido");
 
-    if(document.querySelector(".bebida.box-escolhido")!=null
-    &&document.querySelector(".sobremesa.box-escolhido")!=null)
-    {
+    if (document.querySelector(".bebida.box-escolhido") != null
+        && document.querySelector(".sobremesa.box-escolhido") != null) {
         const botao = document.querySelector(".botao");
-        botao.innerHTML ="Fechar Pedido";
+        botao.innerHTML = "Fechar Pedido";
         const caixabotao = document.querySelector(".botao-fundo");
         caixabotao.classList.add("colorirDeVerde");
     }
 }
-function escolherBebida(element){
+function escolherBebida(element) {
     const selecionado = document.querySelector(".bebida.box-escolhido");
-    if(selecionado!=null){
+    if (selecionado != null) {
         selecionado.classList.remove("box-escolhido");
     }
     element.classList.add("box-escolhido");
 
-    if(document.querySelector(".comida.box-escolhido")!=null
-    &&document.querySelector(".sobremesa.box-escolhido")!=null)
-    {
+    if (document.querySelector(".comida.box-escolhido") != null
+        && document.querySelector(".sobremesa.box-escolhido") != null) {
         const botao = document.querySelector(".botao");
-        botao.innerHTML ="Fechar Pedido";
+        botao.innerHTML = "Fechar Pedido";
         const caixabotao = document.querySelector(".botao-fundo");
         caixabotao.classList.add("colorirDeVerde");
     }
 }
-function escolherSobremesa(element){
+function escolherSobremesa(element) {
     const selecionado = document.querySelector(".sobremesa.box-escolhido");
-    if(selecionado!=null){
+    if (selecionado != null) {
         selecionado.classList.remove("box-escolhido");
     }
     element.classList.add("box-escolhido");
 
-    if(document.querySelector(".comida.box-escolhido")!=null
-    &&document.querySelector(".bebida.box-escolhido")!=null)
-    {
+    if (document.querySelector(".comida.box-escolhido") != null
+        && document.querySelector(".bebida.box-escolhido") != null) {
         const botao = document.querySelector(".botao");
-        botao.innerHTML ="Fechar Pedido";
+        botao.innerHTML = "Fechar Pedido";
         const caixabotao = document.querySelector(".botao-fundo");
         caixabotao.classList.add("colorirDeVerde");
     }
 }
-function stringParaNumero(elemento){
-    let aux="";
-    for(let i=0;i<elemento.length;i++){
-        if(!isNaN(elemento[i])){aux+=elemento[i];}
-        if(elemento[i]==","){aux+=".";}
+function stringParaNumero(elemento) {
+    let aux = "";
+    for (let i = 0; i < elemento.length; i++) {
+        if (!isNaN(elemento[i])) { aux += elemento[i]; }
+        if (elemento[i] == ",") { aux += "."; }
     }
     return Number(aux);
 }
-function confirmacao(){
+
+let comida;
+let bebida;
+let sobremesa;
+let preçoComida;
+let preçoBebida;
+let preçoSobremesa;
+let preçoTotal;
+
+function confirmacao() {
     let element = document.querySelector(".colorirDeVerde")
-    if(element!=null){
+    if (element != null) {
         element = document.querySelector(".caixa-branca").classList.remove("esconder")
     }
-    let comida = document.querySelector(".opcao.comida.box-escolhido :nth-child(2)").innerHTML;
-    let bebida = document.querySelector(".opcao.bebida.box-escolhido :nth-child(2)").innerHTML;
-    let sobremesa = document.querySelector(".opcao.sobremesa.box-escolhido :nth-child(2)").innerHTML;
+    comida = document.querySelector(".opcao.comida.box-escolhido :nth-child(2)").innerHTML;
+    bebida = document.querySelector(".opcao.bebida.box-escolhido :nth-child(2)").innerHTML;
+    sobremesa = document.querySelector(".opcao.sobremesa.box-escolhido :nth-child(2)").innerHTML;
 
-    let preçoComida = document.querySelector(".opcao.comida.box-escolhido :nth-child(4)").innerHTML;
-    let preçoBebida = document.querySelector(".opcao.bebida.box-escolhido :nth-child(4)").innerHTML;
-    let preçoSobremesa = document.querySelector(".opcao.sobremesa.box-escolhido :nth-child(4)").innerHTML;
-    
-    let pcomida=stringParaNumero(preçoComida);
-    let pbebida=stringParaNumero(preçoBebida);
-    let psobremesa=stringParaNumero(preçoSobremesa);
-    console.log(pcomida);
+    preçoComida = document.querySelector(".opcao.comida.box-escolhido :nth-child(4)").innerHTML;
+    preçoBebida = document.querySelector(".opcao.bebida.box-escolhido :nth-child(4)").innerHTML;
+    preçoSobremesa = document.querySelector(".opcao.sobremesa.box-escolhido :nth-child(4)").innerHTML;
 
-    let preçoTotal = pcomida+pbebida+psobremesa;
-    preçoTotal=preçoTotal.toFixed(2);
-    preçoTotal="R$"+preçoTotal;
+    preçoComida = stringParaNumero(preçoComida);
+    preçoBebida = stringParaNumero(preçoBebida);
+    preçoSobremesa = stringParaNumero(preçoSobremesa);
+
+    preçoTotal = preçoComida + preçoBebida + preçoSobremesa;
+    preçoTotal = preçoTotal.toFixed(2);
+    preçoComida = preçoComida.toFixed(2);
+    preçoBebida = preçoBebida.toFixed(2);
+    preçoSobremesa = preçoSobremesa.toFixed(2);
+
+    preçoComida = "R$" + preçoComida;
+    preçoBebida = "R$" + preçoBebida;
+    preçoSobremesa = "R$" + preçoSobremesa;
+    preçoTotal = "R$" + preçoTotal;
 
     const comidaSubstituir = document.querySelector(".caixa-verde :nth-child(2)");
     const bebidaSubstituir = document.querySelector(".caixa-verde :nth-child(4)");
@@ -85,57 +97,41 @@ function confirmacao(){
     const preçoSobremesaSubstituir = document.querySelector(".caixa-verde :nth-child(7)");
     const preçoTotalSubstituir = document.querySelector(".caixa-verde :nth-child(9)")
 
-    comidaSubstituir.innerHTML=comida;
-    bebidaSubstituir.innerHTML=bebida;
-    sobremesaSubstituir.innerHTML=sobremesa;
+    comidaSubstituir.innerHTML = comida;
+    bebidaSubstituir.innerHTML = bebida;
+    sobremesaSubstituir.innerHTML = sobremesa;
 
-    preçoComidaSubstituir.innerHTML=preçoComida;
-    preçoBebidaSubstituir.innerHTML=preçoBebida;
-    preçoSobremesaSubstituir.innerHTML=preçoSobremesa;
-    preçoTotalSubstituir.innerHTML=preçoTotal;
+    preçoComidaSubstituir.innerHTML = preçoComida;
+    preçoBebidaSubstituir.innerHTML = preçoBebida;
+    preçoSobremesaSubstituir.innerHTML = preçoSobremesa;
+    preçoTotalSubstituir.innerHTML = preçoTotal;
 }
-function cancelar(){
+
+function cancelar() {
     const cancelar = document.querySelector(".caixa-branca");
     cancelar.classList.add("esconder");
 }
-function wpp(){
-   
+
+function wpp() {
+
     const comidas = document.querySelectorAll(".opcao");
     let url = "https://wa.me/5531991929224?text=Olá,";
-    let texto ="gostaria de fazer um pedido:\n\- Prato: ";
-    let total=0.0;
-    for(let i=0;i<3;i++){
-        
-      if(comidas[i].classList.contains("box-escolhido")){
-          if(i==0){texto+="Frango Yin Yang\n";total+=14.90;}
-          if(i==1){texto+="Quarteirão\n";total+=12.90;}
-          if(i==2){texto+="Whopper\n";total+=13.90;}
-      }  
-    }
-    texto+="- Bebida: ";
-    for(let i=3;i<6;i++){
-        if(comidas[i].classList.contains("box-escolhido")){
-            if(i==3){texto+="Coquinha Gelada\n";total+=4.90;}
-            if(i==4){texto+="Coca do MC\n";total+=8.90;}
-            if(i==5){texto+="Pepsi do Bk\n";total+=7.90;}
-        }  
-      }
-      texto+="- Sobremesa: ";   
-      for(let i=6;i<9;i++){
-        if(comidas[i].classList.contains("box-escolhido")){
-            if(i==6){texto+="Pudim\n";total+=7.90;}
-            if(i==7){texto+="Top-Sundae\n";total+=8.90;}
-            if(i==8){texto+="Balde-Ovomaltine\n";total+=10.90;}
-        }  
-      }
-      texto+="Total: R$"+total.toFixed(2);
-      const nome =prompt("Digite seu nome: ")
-      const endereço = prompt("Digite seu Endereço: ")
-      texto += "\n\n"+"Nome: "+nome;
-      texto += "\n"+"Endereço: "+endereço;
-      url =url+ encodeURIComponent(texto);
-    
-        window.open(url);
+    let texto = " gostaria de fazer um pedido:\n\- Prato: ";
+    let total = 0.0;
 
-    
+    texto += comida + "\n";
+    total += preçoComida;
+    texto += "- Bebida: " + bebida + "\n";
+    total += preçoBebida;
+    texto += "- Sobremesa: " + sobremesa + "\n";
+    total += preçoSobremesa;
+    texto += "Total: " + preçoTotal;
+
+    const nome = prompt("Digite seu nome: ")
+    const endereço = prompt("Digite seu Endereço: ")
+    texto += "\n\n" + "Nome: " + nome;
+    texto += "\n" + "Endereço: " + endereço;
+    url = url + encodeURIComponent(texto);
+
+    window.open(url);
 }
